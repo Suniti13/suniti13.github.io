45c78ff27f49c57b66eff7be06a06f9322a4b178
Suniti13 <sunitigaonkar14@gmail.com>
2020-08-14 03:07:51 +0530

    nav update

--- js/index.js
+++ js/index.js
@@ -1,17 +1,20 @@
 const menuBtn = document.querySelector('.menu-btn');
-const menuBtnWrapper = document.querySelector('.menu-btn-wrapper');
+/*const menuBtnWrapper = document.querySelector('.menu-btn-wrapper');*/
 const overlayNav = document.getElementById('overlay-nav');
+const logoContainer = document.querySelector('.logo-container')
 
 let menuOpen = false;
 menuBtn.addEventListener('click', () =>{
     if(!menuOpen) {
-        menuBtnWrapper.classList.add('menu-active');
+        /*menuBtnWrapper.classList.add('menu-active');*/
+        logoContainer.classList.add('menu-active-logo');
         menuBtn.classList.add('open');
         overlayNav.style.height = "100%";
         menuOpen = true;
     }
     else {
-        menuBtnWrapper.classList.remove('menu-active');
+        /*menuBtnWrapper.classList.remove('menu-active');*/
+        logoContainer.classList.remove('menu-active-logo');
         menuBtn.classList.remove('open');
         overlayNav.style.height = "0";
         menuOpen = false;
