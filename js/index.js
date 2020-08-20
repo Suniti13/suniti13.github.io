aae7982dffbbe8dbd6e51daa0639c34be99edb1a
Suniti13 <sunitigaonkar14@gmail.com>
2020-08-20 23:12:39 +0530

    added skills

--- js/index.js
+++ js/index.js
@@ -4,6 +4,11 @@ const overlayNav = document.getElementById('overlay-nav');
 const logoContainer = document.querySelector('.logo-container');
 const main_container = document.querySelector('.main');
 
+const homeNav = document.getElementsByClassName('home-nav');
+const projectsNav = document.getElementsByClassName('projects-nav');
+const aboutNav = document.getElementsByClassName('about-nav');
+const contactNav = document.getElementsByClassName('contact-nav');
+
 let menuOpen = false;
 menuBtn.addEventListener('click', () =>{
     if(!menuOpen) {
@@ -21,3 +26,20 @@ menuBtn.addEventListener('click', () =>{
         menuOpen = false;
     }
 });
+
+for(var i=0; i<homeNav.length; i++) {
+    projectsNav[i].addEventListener('click', () => {
+        logoContainer.classList.remove('menu-active-logo');
+        menuBtn.classList.remove('open');
+        overlayNav.style.height = "0";
+        menuOpen = false;
+    });
+
+    contactNav[i].addEventListener('click', () => {
+        logoContainer.classList.remove('menu-active-logo');
+        menuBtn.classList.remove('open');
+        overlayNav.style.height = "0";
+        menuOpen = false;
+    });
+
+}
